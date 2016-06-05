98c827bee0963c4f698b2684b894e7a4af677f62
KiSoo <coderkisoo@gmail.com>
2016-06-05 19:43:48 +0800

    增加了litepal框架

--- app/src/main/java/com/help/app/APP.java
+++ app/src/main/java/com/help/app/APP.java
@@ -1,13 +1,21 @@
 package com.help.app;
 
-import android.app.Application;
+import com.help.model.bean.HelpContact;
+
+import org.litepal.LitePalApplication;
+
+import java.util.List;
 
 /**
  * Created by gan on 2016/6/1.
  */
-public class APP extends Application{
+public class APP extends LitePalApplication {
+    public static List<HelpContact> contacts;
+
     @Override
     public void onCreate() {
         super.onCreate();
+        LitePalApplication.initialize(this);
+
     }
 }
